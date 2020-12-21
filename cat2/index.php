f6bb6339c141cb78f9ba11a41a0dc245907cf617
Benoit DAVID <benoit.davi@free.fr>
2020-12-21 12:28:06 +0000

    améliorations

---
+++ cat2/index.php
@@ -0,0 +1,27 @@
+<?php
+/*PhpDoc:
+name: index.php
+title: cat2/index.php - Gestion du catalogue des cartes du Shom v2
+classes:
+doc: |
+  
+journal: |
+  13/12/2020:
+    - passage en V2
+*/
+?>
+<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>cat2</title></head><body>
+<h2>Cat2</h2><ul>
+<li><a href='mapcat.inc.php?action=yaml'>Affiche le catalogue en Yaml</a></li>
+<li><a href='http://localhost/schema/?action=check&file=../geoapi/shomgt/cat2/mapcat.yaml'>
+  Vérifie la conformité du catalogue à son schéma</a></li>
+<li><a href='mapcat.inc.php?action=html'>Affiche le catalogue en Html</a></li>
+<li><a href='llmap.php'>Affiche le catalogue sous la forme d'une carte</a></li>
+<li><a href='gestion.php?action=compCat'>
+  Détecte de nouvelles cartes ou des cartes périmées par confontation du catalogue au flux WFS</a></li>
+<li><a href='hgan.php?action=updt'>Liste les cartes à mettre à jour ordonnées par age décroissant</a></li>
+</ul>
+<h3>Autres actions</h3><ul>
+  <li><a href='gestion.php'>Suivi du flux WFS</a></li>
+  <li><a href='hgan.php'>Suivi des GAN</a></li>
+</ul>
