60666ed921547964ba3f93f5528e83e22eb51bce
Benoit DAVID <benoit.davi@free.fr>
2022-06-24 16:49:33 +0000

    amélioration de la doc du tableau des degres de péremption

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,5 +1,5 @@
 <?php
-// dashboard/index.php
+// dashboard/index.php - 24/6/2022
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../sgserver/lib/mapversion.inc.php';
@@ -12,7 +12,7 @@ echo "<!DOCTYPE HTML><html><head><title>dashboard</title></head><body>\n";
 
 if (!isset($_GET['a'])) {
   echo "<h2>Menu:</h2><ul>\n";
-  echo "<li><a href='?a=listOfInterest'>listOfInterest</li>\n";
+  //echo "<li><a href='?a=listOfInterest'>listOfInterest</li>\n";
   echo "<li><a href='?a=newObsoleteMaps'>Nouvelles cartes et cartes obsolètes dans le portefeuille par rapport au WFS</li>\n";
   echo "<li><a href='?a=perempt'>Degrés de péremption des cartes du portefeuille</li>\n";
   echo "</ul>\n";
@@ -102,7 +102,7 @@ class MapFromWfs {
   }
 };
 
-class Portfolio { // Portefeuille des cartes exposées sur ShomGt
+class Portfolio { // Portefeuille des cartes exposées sur ShomGt issu de maps.json
   static array $all; // contenu du fichier maps.json
   
   static function init(): void {
@@ -176,7 +176,7 @@ if ($_GET['a'] == 'newObsoleteMaps') { // détecte de nouvelles cartes à ajoute
   }
 }
 
-class GanInSet {
+class GanInSet { // cartouche d'une carte 
   protected string $title;
   protected array $spatial; // sous la forme ['SW'=> sw, 'NE'=> ne]
   
@@ -196,7 +196,7 @@ class GanInSet {
   }
 };
 
-class Gan {
+class Gan { // chargement de la synthèse des GANs par carte 
   const GAN_DIR = __DIR__.'/gan';
   const PATH = __DIR__.'/gans.'; // chemin des fichiers stockant la synthèse en pser ou en yaml, lui ajouter l'extension
   const PATH_PSER = self::PATH.'pser'; // chemin du fichier stockant le catalogue en pser
@@ -204,6 +204,7 @@ class Gan {
   // le champ édition du GAN comporte des erreurs qui perturbent le TdB, ci-dessous corrections
   // Il se lit {{num}=> [{edACorriger}=> {edCorrigée}]}
   // Liste d'écarts transmise le 15/6/2022 au Shom
+  // Les écarts ci-dessous sont ceux restants après corrections du Shom
   const CORRECTIONS = [
     '6942'=> ["Edition n°3 - 2015"=> "Edition n°3 - 2016"],
     '7143'=> ["Edition n°2 - 2002"=> "Edition n°2 - 2003"],
@@ -214,7 +215,7 @@ class Gan {
     '7593'=> ["Publication 2002"=> "Publication 2003"],
     '7755'=> ["Publication 2015"=> "Publication 2016"],
   ];
-  
+
   static string $hvalid=''; // intervalles des dates de la moisson des GAN
   static array $gans=[]; // dictionnaire [$mapnum => Gan]
   
@@ -229,6 +230,11 @@ class Gan {
   protected string $valid; // date de moissonnage du GAN en format ISO
   protected string $harvestError; // erreur éventuelle du moissonnage
 
+  static function week(string $modified): string { // transforme une date en semaine sur 4 caractères comme utilisé par le GAN 
+    $time = strtotime($modified);
+    return substr(date('o', $time), 2) . date('W', $time);
+  }
+  
   static function init(): void {
     $contents = unserialize(file_get_contents(self::PATH_PSER));
     self::$hvalid = $contents['valid'];
@@ -268,7 +274,7 @@ class Gan {
   }
 };
 
-class Mapcat {
+class Mapcat { // chargement d'un extrait de mapcat.yaml
   protected string $title;
   protected array $mapsFrance;
   
@@ -308,7 +314,7 @@ class Mapcat {
   }
 };
 
-class Perempt {
+class Perempt { // croisement entre le portfeuille et les GANs en vue d'afficher le tableau des degrés de péremption
   protected string $mapNum;
   protected string $pfVersion; // info du portefeuille 
   protected string $pfModified; // info du portefeuille 
@@ -317,7 +323,7 @@ class Perempt {
   protected float $degree; // degré de péremption
   static array $all; // [mapNum => Perempt]
 
-  static function init(): void {
+  static function init(): void { // construction à partir du portefeuille 
     foreach (Portfolio::$all as $mapnum => $map) {
       if ($map['status'] <> 'ok') continue;
       self::$all[$mapnum] = new self($mapnum, $map);
@@ -331,7 +337,7 @@ class Perempt {
     $this->pfModified = substr($map['modified'], 0, 10);
   }
   
-  function setGan(Gan $gan): void {
+  function setGan(Gan $gan): void { // Mise à jour de perempt à partir du GAN
     $this->ganVersion = $gan->version();
     $this->ganCorrections = $gan->corrections;
     $this->degree = $this->degree();
@@ -340,7 +346,7 @@ class Perempt {
   function title(): string { return MapCat::item($this->mapNum)->title(); }
   function mapsFrance(): array { return MapCat::item($this->mapNum)->mapsFrance(); }
     
-  function degree(): float {
+  function degree(): float { // calcul du degré de péremption 
     if (($this->pfVersion == 'undefined') && ($this->ganVersion == 'undefined'))
       return -1;
     $spc = MapCat::item($this->mapNum)->spatialCoeff();
@@ -365,7 +371,7 @@ class Perempt {
       return 100 / $spc;
   }
 
-  static function showAll(): void {
+  static function showAll(): void { // Affichage du tableau des degrés de péremption
     usort(self::$all,
       function(Perempt $a, Perempt $b) {
         if ($a->degree() < $b->degree()) return 1;
@@ -374,10 +380,36 @@ class Perempt {
       });
     //echo "<pre>Perempt="; print_r(Perempt::$all);
     echo "<h2>Degrés de péremption des cartes du portefeuille</h2>\n";
+    echo "<p>On appelle <b>portefeuille</b>, l'ensemble des dernières versions des cartes non obsolètes",
+      " exposées par le serveur de cartes 7z.<br>",
+      "L'objectif du grand tableau ci-dessous est de fournir le degré de péremption des cartes du portefeuille",
+      " mesuré par rapport aux <a href='https://gan.shom.fr/' target='_blank'>GAN (Groupes d'Avis aux Navigateurs)</a>",
+      " qui indiquent chaque semaine notamment les corrections apportées aux cartes.</p>\n";
     echo "<table border=1><tr><td><b>Validité du GAN</b></td><td>",Gan::$hvalid,"</td></tr></table>\n";
+    echo "<h3>Signification des colonnes du tableau ci-dessous</h3>\n";
+    echo "<table border=1>\n";
+    echo "<tr><td><b>#</b></td><td>numéro de la carte</td></tr>\n";
+    echo "<tr><td><b>titre</b></td><td>titre de la carte</td></tr>\n";
+    echo "<tr><td><b>zone géo.</b></td><td>zone géographique dans laquelle se situe la carte</td></tr>\n";
+    echo "<tr><td><b>modif.</b></td><td>date de mise à jour de la dernière version de la carte dans le portefeuille,",
+      " approximée par la date de modification des métadonnées ISO de cette version</td></tr>\n";
+    echo "<tr><td><b>v. Pf</b></td><td>version de la carte dans le portefeuille, identifiée par l'année d'édition de la carte",
+      " suivie du caractère 'c' et du numéro de la dernière correction apportée à la carte</td></tr>\n";
+    echo "<tr><td><b>v. GAN</b></td><td>version de la carte trouvée dans les GANs à la date de validité ci-dessus ;",
+      " le lien permet de consulter les GANs du Shom pour cette carte</td></tr>\n";
+    echo "<tr><td><b>degré</b></td><td>degré de péremption de la carte exprimant l'écart entre les 2 versions ci-dessus ;",
+      " la table ci-dessous est triée par degré décroissant ; ",
+      " l'objectif de gestion est d'éviter les degrés supérieurs ou égaux à 5</td></tr>\n";
+    echo "<tr><td><b>corrections</b></td><td>liste des corrections reconnues dans les GANs, avec en première colonne ",
+      " le numéro de la correction et, dans la seconde colonne, avant le tiret, le no de semaine de la correction",
+      " (année sur 2 caractères et no de semmaine sur 2 caractères)",
+      " et après le tiret le numéro d'avis dans le GAN de cette semaine.</td></tr>\n";
+    echo "</table></p>\n";
+    echo "<p>Attention, certains écarts de version sont dus à des informations incomplètes ou incorrectes",
+      " sur les sites du Shom</p>\n";
     echo "<table border=1>",
-         "<th>num</th><th>title</th><th>maps</th><th>pfModified</th><th>pfVersion</th>",
-         "<th>ganVersion</th><th>degré</th><th>corrections</th>\n";
+         "<th>#</th><th>titre</th><th>zone géo.</th><th>modif.</th><th>v. Pf</th>",
+         "<th>v. GAN</th><th>degré</th><th>corrections</th>\n";
     foreach (Perempt::$all as $p) {
       $p->showAsRow();
     }
@@ -385,13 +417,15 @@ class Perempt {
     
   }
   
-  function showAsRow(): void {
+  function showAsRow(): void { // Affichage d'une ligne du tableau des degrés de péremption
     echo "<tr><td>$this->mapNum</td>";
     echo "<td>",$this->title(),"</td>";
     echo "<td>",implode(', ', $this->mapsFrance()),"</td>";
     echo "<td>$this->pfModified</td>";
     echo "<td>$this->pfVersion</td>";
-    echo "<td>$this->ganVersion</td>";
+    $ganWeek = Gan::week($this->pfModified);
+    $href = "https://gan.shom.fr/diffusion/qr/gan/$this->mapNum/$ganWeek";
+    echo "<td><a href='$href' target='_blank'>$this->ganVersion</a></td>";
     printf("<td>%.2f</td>", $this->degree);
     echo "<td><table border=1>";
     foreach ($this->ganCorrections as $c) {
@@ -404,11 +438,11 @@ class Perempt {
 };
 
 if ($_GET['a'] == 'perempt') {
-  Portfolio::init();
-  MapCat::init();
-  Gan::init();
+  Portfolio::init(); // initialisation à partir du portefeuille
+  MapCat::init(); // chargement du fichier mapcat.yaml
+  Gan::init(); // chargement de la synthèse des GANs
   //echo "<pre>Gan="; print_r(Gan::$gans);
-  Perempt::init();
+  Perempt::init(); // construction à partir du portefeuille
   // Mise à jour de perempt à partir du GAN
   foreach (Perempt::$all as $mapNum => $perempt) {
     if (!($gan = Gan::item($mapNum)))
@@ -416,5 +450,5 @@ if ($_GET['a'] == 'perempt') {
     else
       $perempt->setGan($gan);
   }
-  Perempt::showAll();
+  Perempt::showAll(); // Affichage du tableau des degrés de péremption
 }
