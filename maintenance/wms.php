5d8c241d3c75e19317b550c8425344a7de7196bf
Benoit DAVID <benoit.davi@free.fr>
2023-08-05 05:54:03 +0000

    ajout site de maintenance

---
+++ maintenance/wms.php
@@ -0,0 +1,5 @@
+<?php
+// maintenance/wms.php
+header('HTTP/1.1 503 Service Unavailable');
+header('Content-type: text/plain; charset="utf-8"');
+die("Site en maintenance, service non disponible\n");
